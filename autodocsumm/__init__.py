de87cf3cce519b859b0b974ccd7eeaf6b1cbb1d3
Chilipp <p.sommer@posteo.de>
2018-09-20 15:22:37 +0200

    Updated version

--- autodocsumm/__init__.py
+++ autodocsumm/__init__.py
@@ -43,7 +43,7 @@ except ImportError:
 if six.PY2:
     from itertools import imap as map
 
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 
 __author__ = "Philipp Sommer"
 
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 
 setup(name='autodocsumm',
-      version='0.1.6',
+      version='0.1.7',
       description='Extended sphinx autodoc including automatic autosummaries',
       long_description=readme(),
       classifiers=[
